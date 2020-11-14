5a3e8032e09162a8a6a6cb3caa9c4687b6c5f070
zy136946818 <136946818@qq.com>
2020-11-14 11:35:50 +0800

    修复缺少jar包问题

--- src/main/java/com/neuedu/utlis/CookieUtils.java
+++ src/main/java/com/neuedu/utlis/CookieUtils.java
@@ -1,12 +1,13 @@
 package com.neuedu.utlis;
 
+
 import javax.servlet.http.Cookie;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CookieUtils {
     public static Map<String, Cookie> getCookie(Cookie[] cookies){
-        Map<String ,Cookie> maps= new HashMap<>();
+        Map<String , Cookie> maps= new HashMap<>();
         if (cookies!=null){
             for (Cookie c:cookies){
                 maps.put(c.getName(),c);
